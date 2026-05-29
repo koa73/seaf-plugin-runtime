da231072b820c89b139d21357bfee3b349b019f7
oleg <koa73@bk.ru>
2026-05-29 20:01:29 +0300

    #0.5.88

--- plugin/seaf.plugin.js
+++ plugin/seaf.plugin.js
@@ -1,6 +1,6 @@
 /**
  * SEAF plugin for draw.io desktop runtime.
- * Runtime script version: 0.5.87
+ * Runtime script version: 0.5.88
  * Uses main-process IPC for config, command execution and logs.
  */
 Draw.loadPlugin(function(ui)
